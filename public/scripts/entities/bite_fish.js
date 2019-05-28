1504746dbf0105bc8ddb92b8e047d56bb12c534b
venetrius <gergelygjuhasz@gmail.com>
2019-05-28 09:05:34 -0600

    add bite fish

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,7 @@
+class BiteFish extends Fish {
+
+  constructor(options) {
+    super(options); // Call super to run the code inside `Fish`'s constructor
+    this.imageUri = '/images/bite-fish.gif'; // Set the image
+  }
+}
